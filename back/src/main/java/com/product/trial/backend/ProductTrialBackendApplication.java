826cb57174d58343e18e2fc5add14123ceb71eaa
AMARA sofiane <amarasofiane94@gmail.com>
2025-06-21 20:53:26 +0100

    init project

---
+++ back/src/main/java/com/product/trial/backend/ProductTrialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.product.trial.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTrialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductTrialBackendApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/product/trial/backend/ProductTrialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.trial.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTrialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
